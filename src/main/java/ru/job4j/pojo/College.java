3e15a84b900749c78bd8d91ced70ca3d8266e706
sdgd0737 <d0737@ya.ru>
2023-01-28 07:52:24 +0300

    2. Модель данных. [#174683]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,12 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setFullName("Иванов Иван Иванович");
+        student.setGroup("A1");
+        student.setDateOfReceipt(new Date());
+    }
+}
